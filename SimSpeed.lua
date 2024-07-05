40aeba91ded3d11cb0218740eb49ee6c73f96e4a
Owen <paradigmsort>
2024-07-05 08:44:46 -0700

    syntax fixes

--- SimSpeed.lua
+++ SimSpeed.lua
@@ -24,12 +24,12 @@ OnAnyLoad {
     function ()
         AdjustedSimSpeed = config.TargetSimSpeed
         LerpFinishesAtTime = nil
-        while true
-            if LerpFinishesAtTime ~= nil && _worldTime > LerpFinishesAtTime then
+        while true do
+            if LerpFinishesAtTime ~= nil and _worldTime > LerpFinishesAtTime then
                 LerpFinishesAtTime = nil
             end
             if LerpFinishesAtTime ~= nil then
-                if AdjustedSimSpeed != config.TargetSimSpeed then
+                if AdjustedSimSpeed ~= config.TargetSimSpeed then
                     print("Refresh with active adjustment:", AdjustedSimSpeed)
                 end
                 -- No active lerp; refresh to ensure the timer doesn't run out
