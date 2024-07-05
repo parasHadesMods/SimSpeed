516dc151a8eb9874ede2de913c0f94429254cd21
Owen <paradigmsort>
2024-07-05 08:38:54 -0700

    initial commit

---
+++ SimSpeed.lua
@@ -0,0 +1,44 @@
+local config = {
+    TargetSimSpeed = 0.5
+}
+
+local RealAdjustSimulationSpeed = AdjustSimulationSpeed
+local AdjustedSimSpeed = config.TargetSimSpeed
+local LerpFinishesAtTime = nil
+
+AdjustSimulationSpeed = function(args)
+    AdjustedSimSpeed = args.Fraction * config.TargetSimSpeed
+
+    if args.LerpTime then
+        LerpFinishesAtTime = _worldTime + args.LerpTime
+    else
+        LerpFinishesAtTime = nil
+    end
+    RealAdjustSimulationSpeed({
+        Fraction = AdjustedSimSpeed,
+        LerpTime = args.LerpTime
+    })
+end
+
+OnAnyLoad {
+    function ()
+        AdjustedSimSpeed = config.TargetSimSpeed
+        LerpFinishesAtTime = nil
+        while true
+            if LerpFinishesAtTime ~= nil && _worldTime > LerpFinishesAtTime then
+                LerpFinishesAtTime = nil
+            end
+            if LerpFinishesAtTime ~= nil then
+                if AdjustedSimSpeed != config.TargetSimSpeed then
+                    print("Refresh with active adjustment:", AdjustedSimSpeed)
+                end
+                -- No active lerp; refresh to ensure the timer doesn't run out
+                RealAdjustSimulationSpeed({
+                    Fraction = AdjustedSimSpeed,
+                    LerpTime = 0
+                })
+            end
+            wait(.1)
+        end
+    end
+}
